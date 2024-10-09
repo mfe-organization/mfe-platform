bd661eb93d6641f5903117a8bec7738fb3dcf840
Dzmitry <ignatovich.dm@gmail.com>
2024-10-09 14:17:12 +0200

    updated vite config

--- Dev.Dockerfile
+++ Dev.Dockerfile
@@ -9,8 +9,4 @@ RUN npm install
 
 COPY . .
 
-RUN npm run build
-
 EXPOSE 3001
-
-CMD ["npm", "run", "dev", "--", "--host", "0.0.0.0", "--port", "3001"]
--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,39 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import federation from '@originjs/vite-plugin-federation';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    federation({
-      name: "platform",
-      remotes: {
-        frontendA: 'http://localhost:3002/assets/remoteEntry.js',
-        frontendB: 'http://localhost:3003/assets/remoteEntry.js',
-      },
-      shared: [
-        "react",
-        "react-dom",
-        "react-router-dom",
-      ],
-    }),
-  ],
-  server: {
-    port: 3001,
-  },
-  build: {
-    modulePreload: false,
-    target: 'esnext',
-    minify: false,
-    cssCodeSplit: false
+export default defineConfig(({ mode }) => {
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+
+  if (mode !== 'development' && (!process.env.VITE_FRONTEND_A_URL || !process.env.VITE_FRONTEND_B_URL)) {
+    throw new Error('VITE_FRONTEND_A_URL or VITE_FRONTEND_B_URL are not defined in the environment');
+  }
+
+  return {
+    plugins: [
+      react(),
+      federation({
+        name: "platform",
+        remotes: {
+          frontendA: `${mode === "development" ? 'http://localhost:3002' : process.env.VITE_FRONTEND_A_URL}/assets/remoteEntry.js`,
+          frontendB: `${mode === "development" ? 'http://localhost:3002' : process.env.VITE_FRONTEND_B_URL}/assets/remoteEntry.js`,
+        },
+        shared: [
+          "react",
+          "react-dom",
+          "react-router-dom",
+        ],
+      }),
+    ],
+    server: {
+      port: 3001,
+    },
+    build: {
+      modulePreload: false,
+      target: 'esnext',
+      minify: false,
+      cssCodeSplit: false
+    }
   }
 })
+
